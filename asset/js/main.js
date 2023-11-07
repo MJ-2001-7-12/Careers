e24b622a97b1a90dbf06d939e07208234bf2a6b9
Merin P Jose <merin.jose@experionglobal.local>
2023-11-07 17:21:59 +0530

    feat:-upadted form elements with data from career list elements

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,134 +1,94 @@
 
-//Dynamic 
+document.addEventListener('DOMContentLoaded', function () {
+    const careersBoxContainer = document.getElementById('careers-box-container');
 
-// const request = fetch('https://dummyjson.com/users');
-// console.log(request);
+    // Function to populate form fields and navigate to edit.html
+    function handleCareerBoxClick(event) {
+        if (event.target && event.target.tagName === 'BUTTON') {
+            const selectedCareer = event.target.closest('.career-box');
+            if (selectedCareer) {
+                const firstName = selectedCareer.querySelector('h3').textContent;
+                const lastName = selectedCareer.querySelector('.last-name').textContent.replace('Last Name: ', '');
+                const maidenName = selectedCareer.querySelector('.maiden-name').textContent.replace('Maiden Name: ', '');
+                const age = selectedCareer.querySelector('.age').textContent.replace('Age: ', '');
+                const gender = selectedCareer.querySelector('.gender').textContent.replace('Gender: ', '');
+                const birthDate = selectedCareer.querySelector('.b-date').textContent.replace('Birth Date: ', '');
 
-
-// let a = [];
-
-
-// request
-//     .then(res => res.json())
-//     .then((data) => {
-//         console.log(data)
-//         data.users.forEach((user, index) => {
-//             if (index < 4) {
-//                 console.log(user.firstName);
-//                 a.push(user);
-//                 console.log(a[index].firstName)
-//             }
-//         })
-
-
-//         document.getElementById("d-name-1").textContent = `${a[0].firstName}`;
-//         document.getElementById("d-name-2").textContent = `${a[1].firstName}`;
-//         document.getElementById("d-name-3").textContent = `${a[2].firstName}`;
-//         document.getElementById("d-name-4").textContent = `${a[3].firstName}`;
-//         document.getElementById("d-img-1").src = `${a[0].image}`;
-//         document.getElementById("d-img-2").src = `${a[2].image}`;
-//         document.getElementById("d-img-3").src = `${a[3].image}`;
-//         document.getElementById("d-img-4").src = `${a[1].image}`;
-//     })
-
-
-
-
-
-//Careers Page    
-
-
-const request = fetch('https://dummyjson.com/users');
-//console.log(request);
-
-
-const courseBox = document.createElement("div");
-
-request
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data.users[1])
-
-        const careersBoxContainer = document.getElementById('careers-box-container');
-
-        for (i = 0; i < 10; i++) {
-
-            //Creating Elements
-            let careerBox = document.createElement("div");
-            let careerBoxLeftSide = document.createElement("div");
-            let careerBoxRightSide = document.createElement("div");
-
-
-
-            let careerBoxNewBadge = document.createElement('span');
-            let careerBoxFirstName = document.createElement('h3');
-
-            //Other info
-            let careerBoxOtherInfo = document.createElement('div');
-            let careerBoxLastName = document.createElement('p');
-            let careerBoxMaidenName = document.createElement('p');
-            let careerBoxAge = document.createElement('p');
-            let careerBoxGender = document.createElement('p');
-            let careerBoxBirthDate = document.createElement('p');
-
-            //Apply btn
-            let careerBoxBtn = document.createElement('button')
-
-
-            //Adding class names
-            careerBox.classList.add('career-box');
-            careerBoxLeftSide.classList.add('left-side');
-            careerBoxRightSide.classList.add('right-side');
-
-            careerBoxNewBadge.classList.add('new');
-
-            //Other info
-            careerBoxOtherInfo.classList.add('other-info');
-
-            // careerBoxLastName.classList.add('last-name');
-            // careerBoxMaidenName.classList.add('maiden-name');
-            // careerBoxAge.classList.add('age');
-            // careerBoxGender.classList.add('gender');
-            // careerBoxBirthDate.classList.add('b-date');
-
-
-            //Adding data
-            careerBoxNewBadge.textContent = "New"
-            careerBoxFirstName.textContent = data.users[i].firstName;
-            careerBoxLastName.textContent = "Last Name: " + data.users[i].lastName;
-            careerBoxMaidenName.textContent = "Maiden Name: " + data.users[i].maidenName;
-            careerBoxAge.textContent = "Age: " + data.users[i].age;
-            careerBoxGender.textContent = "Gender: " + data.users[i].gender;
-            careerBoxBirthDate.textContent = "Birth Date: " + data.users[i].birthDate;
-
-            careerBoxBtn.textContent = "Apply Now"
-
-
-            //Main Container
-            careersBoxContainer.appendChild(careerBox);
-
-            //Course container
-            careerBox.appendChild(careerBoxLeftSide);
-            careerBox.appendChild(careerBoxRightSide);
-
-
-            //left side
-            careerBoxLeftSide.appendChild(careerBoxNewBadge);
-            careerBoxLeftSide.appendChild(careerBoxFirstName);
-            careerBoxLeftSide.appendChild(careerBoxOtherInfo);
-
-            //other info
-            careerBoxOtherInfo.appendChild(careerBoxLastName);
-            careerBoxOtherInfo.appendChild(careerBoxMaidenName);
-            careerBoxOtherInfo.appendChild(careerBoxAge);
-            careerBoxOtherInfo.appendChild(careerBoxGender);
-            careerBoxOtherInfo.appendChild(careerBoxBirthDate);
-
-
-            //Right Side
-            careerBoxRightSide.appendChild(careerBoxBtn);
-
+                const queryString = `?firstName=${firstName}&lastName=${lastName}&maidenName=${maidenName}&age=${age}&gender=${gender}&birthDate=${birthDate}`;
+                window.location.href = `edit.html${queryString}`;
+            }
         }
+    }
 
+    // Attach the click event to the careersBoxContainer
+    careersBoxContainer.addEventListener('click', handleCareerBoxClick);
 
-    })
+    // Fetch career data
+    fetch('https://dummyjson.com/users')
+        .then(res => res.json())
+        .then((data) => {
+            for (let i = 0; i < 10; i++) {
+                let careerBox = document.createElement("div");
+                let careerBoxLeftSide = document.createElement("div");
+                let careerBoxRightSide = document.createElement("div");
+
+                let careerBoxNewBadge = document.createElement('span');
+                let careerBoxFirstName = document.createElement('h3');
+
+                // Other info
+                let careerBoxOtherInfo = document.createElement('div');
+                let careerBoxLastName = document.createElement('p');
+                let careerBoxMaidenName = document.createElement('p');
+                let careerBoxAge = document.createElement('p');
+                let careerBoxGender = document.createElement('p');
+                let careerBoxBirthDate = document.createElement('p');
+
+                // Apply btn
+                let careerBoxBtn = document.createElement('button');
+
+                // Adding class names
+                careerBox.classList.add('career-box');
+                careerBoxLeftSide.classList.add('left-side');
+                careerBoxRightSide.classList.add('right-side');
+
+                careerBoxNewBadge.classList.add('new');
+                careerBoxLastName.classList.add('last-name'); 
+                careerBoxMaidenName.classList.add('maiden-name'); 
+                careerBoxAge.classList.add('age'); 
+                careerBoxGender.classList.add('gender'); 
+                careerBoxBirthDate.classList.add('b-date');  
+                // Adding data
+                careerBoxNewBadge.textContent = "New";
+                careerBoxFirstName.textContent = data.users[i].firstName;
+                careerBoxLastName.textContent = "Last Name: " + data.users[i].lastName;
+                careerBoxMaidenName.textContent = "Maiden Name: " + data.users[i].maidenName;
+                careerBoxAge.textContent = "Age: " + data.users[i].age;
+                careerBoxGender.textContent = "Gender: " + data.users[i].gender;
+                careerBoxBirthDate.textContent = "Birth Date: " + data.users[i].birthDate;
+
+                careerBoxBtn.textContent = "Apply Now";
+
+                // Main Container
+                careersBoxContainer.appendChild(careerBox);
+
+                // Course container
+                careerBox.appendChild(careerBoxLeftSide);
+                careerBox.appendChild(careerBoxRightSide);
+
+                // Left side
+                careerBoxLeftSide.appendChild(careerBoxNewBadge);
+                careerBoxLeftSide.appendChild(careerBoxFirstName);
+                careerBoxLeftSide.appendChild(careerBoxOtherInfo);
+
+                // Other info
+                careerBoxOtherInfo.appendChild(careerBoxLastName);
+                careerBoxOtherInfo.appendChild(careerBoxMaidenName);
+                careerBoxOtherInfo.appendChild(careerBoxAge);
+                careerBoxOtherInfo.appendChild(careerBoxGender);
+                careerBoxOtherInfo.appendChild(careerBoxBirthDate);
+
+                // Right Side
+                careerBoxRightSide.appendChild(careerBoxBtn);
+            }
+        });
+});
